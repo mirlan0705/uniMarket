39fe3516d9d885e45d1cb05e19cd341a717866f4
Beatriz Mauhay <beatrizmabu@gmail.com>
2026-03-31 14:22:27 +0100

    Add files via upload

--- Coursework/js/unimarket.js
+++ Coursework/js/unimarket.js
@@ -1,13 +1,23 @@
+// open/close the sidebar menu
 function toggleMenu() {
     const sidebar = document.getElementById("sidebar");
 
     if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-270px";
+        sidebar.style.left = "-280px";
     } else {
         sidebar.style.left = "0px";
     }
 }
+// expand/collapse a category section and change the arrow icon direction
+function toggleCategory(element) {
+    const category = element.parentElement;
+    category.classList.toggle("active");
 
-function toggleCategory(element){
-    element.parentElement.classList.toggle("active");
+    const arrow = category.querySelector(".arrow-icon");
+
+    if (category.classList.contains("active")) {
+        arrow.src = "images/arrowup.png";
+    } else {
+        arrow.src = "images/arrowdown.png";
+    }
 }
