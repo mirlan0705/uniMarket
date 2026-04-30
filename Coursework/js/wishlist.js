7c22d88d7dd5cfcb1e7362324d45b165a4c70752
Bea <beatrizmabu@gmail.com>
2026-04-30 13:22:35 +0100

    Update wishlist.js

--- Coursework/js/wishlist.js
+++ Coursework/js/wishlist.js
@@ -1,12 +1,9 @@
-// fills the heart icon when clicked 
+//set the heart as filled, toggle functonality
 function toggleWishlist(element) {
-
     if (element.classList.contains('fa-regular')) {
         element.classList.remove('fa-regular');
         element.classList.add('fa-solid');
-        
     } else {
-        
         element.classList.remove('fa-solid');
         element.classList.add('fa-regular');
     }
