31deccc8541cfc1fec5975a86ee17ad9639503ba
Bea <beatrizmabu@gmail.com>
2026-05-01 14:02:18 +0100

    handle subcategory dropdown based on selected category

--- Coursework/js/sellnow.js
+++ Coursework/js/sellnow.js
@@ -1,5 +1,73 @@
 // For sell now page
 // Photo upload
+
+// stores all categories and their related subcategories (Items detail section)
+const subcategories = {
+  "Computing & Technology": [
+    "Laptops",
+    "Audio",
+    "Accessories",
+    "Chargers"
+  ],
+  "Study & Course Materials": [
+    "Textbooks",
+    "Stationery",
+    "Lab Equipment"
+  ],
+  "Uni Style & Clothes": [
+    "Everyday Wear",
+    "Outerwear & Jackets",
+    "Formal & Event Wear",
+    "Sports Wear",
+    "Accessories"
+  ],
+  "Home & Halls": [
+    "Kitchen",
+    "Bedroom",
+    "Lighting/Decor",
+    "Bathroom",
+    "Cleaning Supplies"
+  ],
+  "Entertainment & Leisure": [
+    "Games",
+    "Board Games & Puzzles",
+    "Musical Instruments"
+  ]
+};
+
+// Handles subcategory dropdown based on selected category
+const categorySelect = document.getElementById("item-category");
+const subcategorySelect = document.getElementById("item-sub-category");
+
+categorySelect.addEventListener("change", function () {
+
+ //get the selected category
+  const selectedCategory = this.value;
+
+  // reset subcategory
+  subcategorySelect.innerHTML =
+    '<option value="" disabled selected>Select a subcategory</option>';
+
+  if (subcategories[selectedCategory]) {
+
+     // enable the subcategory dropdown
+    subcategorySelect.disabled = false;
+
+    // loop through each subcategory for the selected category
+    subcategories[selectedCategory].forEach(sub => {
+
+      //create a new option 
+      const option = document.createElement("option");
+      option.value = sub;
+      option.textContent = sub;
+      subcategorySelect.appendChild(option);
+
+    });
+  } else {
+    subcategorySelect.disabled = true;
+  }
+});
+
 // expand/collapse a category section and change the arrow icon direction (auto closes category)
 function toggleCategory(element) {
     const currentCategory = element.parentElement;
