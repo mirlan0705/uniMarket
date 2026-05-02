e1af641d1a3c8a74fa3639b7b696518b9b0fa2fd
Bea <beatrizmabu@gmail.com>
2026-05-02 18:22:49 +0100

    Added wishlist toggle and test empty state
    toggle wishlist between empty and filled states and test by removing product cards <div id="productcard" class="productcard">

--- Coursework/js/wishlist.js
+++ Coursework/js/wishlist.js
@@ -1,4 +1,24 @@
-// expand/collapse a category section and change the arrow icon direction (auto closes category)
+// handles wishlist display between empty wishlist and product grid based on whether any wishlist items exist. added by bea
+function checkWishlist() {
+  const products = document.querySelectorAll(".productcard");
+  const emptywishlist = document.getElementById("emptywishlist");
+  const grid = document.getElementById("productgrid");
+  const toolbar = document.getElementById("toolbar");
+
+  if (products.length === 0) {
+    emptywishlist.style.display = "flex";
+    grid.style.display = "none";
+    toolbar.style.display = "none";
+  } else {
+    emptywishlist.style.display = "none";
+    grid.style.display = "grid";
+    toolbar.style.display = "flex";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", checkWishlist);
+
+// expand/collapse a category section and change the arrow icon direction (auto closes category) added by bea
 function toggleCategory(element) {
     const currentCategory = element.parentElement;
     const allCategories = document.querySelectorAll(".category");
@@ -25,8 +45,19 @@ function toggleCategory(element) {
     }
 }
 
-//set the heart as filled, toggle functonality
+//set the heart as filled, toggle functonality added by bea
 function toggleWishlist(element) {
     element.classList.toggle('fa-solid');
     element.classList.toggle('fa-regular');
+}
+
+// add to basket change button state
+function addToBasket(button) {
+    button.classList.toggle("added");
+
+    if (button.classList.contains("added")) {
+        button.textContent = "Added to Basket";
+    } else {
+        button.textContent = "Add to Basket";
+    }
 }
