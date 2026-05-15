c1fda7e5e455b40a8d8e8fcbecc9e2c83132aecc
Bea <beatrizmabu@gmail.com>
2026-05-15 10:51:39 +0100

    added profile user dropdown menu

--- Coursework/js/unimarket.js
+++ Coursework/js/unimarket.js
@@ -129,10 +129,34 @@ window.onload = function () {
     if (rightButtons) {
         if (user) {
             rightButtons.innerHTML = `
-                <span style="color:white; margin: 0 10px;">Hi, ${user.name}</span>
-                <a href="/html/sellnow.html" class="protected-link">Sell Now</a>
-                <a href="#" id="logout-btn">Log Out</a>
-            `;
+                <div class="profile-dropdown">
+                     <button class="profile-btn" id="profile-btn">Hi, ${user.name} ▾</button>
+                 <div class="dropdown-menu" id="dropdown-menu">
+                     <a href="/html/profile.html">Profile</a>
+                     <a href="/html/settings.html">Settings</a>
+                     <a href="#" id="logout-btn">Log Out</a>
+                 </div>
+                 <a href="/html/sellnow.html" class="protected-link sell-btn"> Sell Now</a>
+                 </div>
+               `;
+
+     const profileBtn = document.getElementById('profile-btn');
+     const dropdownMenu = document.getElementById('dropdown-menu');
+
+profileBtn.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('show');
+});
+
+// close dropdown when clicking elsewhere
+document.addEventListener('click', (e) => {
+
+    if (
+        !profileBtn.contains(e.target) &&
+        !dropdownMenu.contains(e.target)
+    ) {
+        dropdownMenu.classList.remove('show');
+    }
+});
             document.getElementById('logout-btn').addEventListener('click', (e) => {
                 e.preventDefault();
                 localStorage.removeItem('user');
